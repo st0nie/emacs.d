ba3c274efb4cbe6173cd701ffeaa621d80d1043f
ston <ston.jia@outlook.com>
2024-03-17 21:14:35 +0800

    use fira-code-mode

--- init.el
+++ init.el
@@ -483,76 +483,10 @@
   (yatemplate-fill-alist))
 
 ;;ligature
-(use-package ligature
-  :ensure t
+(use-package fira-code-mode
   :config
-  ;; Enable the "www" ligature in every possible major mode
-  (ligature-set-ligatures 't '("www"))
-  ;; Enable traditional ligature support in eww-mode, if the
-  ;; `variable-pitch' face supports it
-  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
-  ;; Enable all Cascadia and Fira Code ligatures in programming modes
-  (ligature-set-ligatures 'prog-mode
-                          '(;; == === ==== => =| =>>=>=|=>==>> ==< =/=//=// =~
-							;; =:= =!=
-							("=" (rx (+ (or ">" "<" "|" "/" "~" ":" "!" "="))))
-							;; ;; ;;;
-							(";" (rx (+ ";")))
-							;; && &&&
-							("&" (rx (+ "&")))
-							;; !! !!! !. !: !!. != !== !~
-							("!" (rx (+ (or "=" "!" "\." ":" "~"))))
-							;; ?? ??? ?:  ?=  ?.
-							("?" (rx (or ":" "=" "\." (+ "?"))))
-							;; %% %%%
-							("%" (rx (+ "%")))
-							;; |> ||> |||> ||||> |] |} || ||| |-> ||-||
-							;; |->>-||-<<-| |- |== ||=||
-							;; |==>>==<<==<=>==//==/=!==:===>
-							("|" (rx (+ (or ">" "<" "|" "/" ":" "!" "}" "\]"
-											"-" "=" ))))
-							;; \\ \\\ \/
-							("\\" (rx (or "/" (+ "\\"))))
-							;; ++ +++ ++++ +>
-							("+" (rx (or ">" (+ "+"))))
-							;; :: ::: :::: :> :< := :// ::=
-							(":" (rx (or ">" "<" "=" "//" ":=" (+ ":"))))
-							;; // /// //// /\ /* /> /===:===!=//===>>==>==/
-							("/" (rx (+ (or ">"  "<" "|" "/" "\\" "\*" ":" "!"
-											"="))))
-							;; .. ... .... .= .- .? ..= ..<
-							("\." (rx (or "=" "-" "\?" "\.=" "\.<" (+ "\."))))
-							;; -- --- ---- -~ -> ->> -| -|->-->>->--<<-|
-							("-" (rx (+ (or ">" "<" "|" "~" "-"))))
-							;; *> */ *)  ** *** ****
-							("*" (rx (or ">" "/" ")" (+ "*"))))
-							;; www wwww
-							("w" (rx (+ "w")))
-							;; <> <!-- <|> <: <~ <~> <~~ <+ <* <$ </  <+> <*>
-							;; <$> </> <|  <||  <||| <|||| <- <-| <-<<-|-> <->>
-							;; <<-> <= <=> <<==<<==>=|=>==/==//=!==:=>
-							;; << <<< <<<<
-							("<" (rx (+ (or "\+" "\*" "\$" "<" ">" ":" "~"  "!"
-											"-"  "/" "|" "="))))
-							;; >: >- >>- >--|-> >>-|-> >= >== >>== >=|=:=>>
-							;; >> >>> >>>>
-							(">" (rx (+ (or ">" "<" "|" "/" ":" "=" "-"))))
-							;; #: #= #! #( #? #[ #{ #_ #_( ## ### #####
-							("#" (rx (or ":" "=" "!" "(" "\?" "\[" "{" "_(" "_"
-										 (+ "#"))))
-							;; ~~ ~~~ ~=  ~-  ~@ ~> ~~>
-							("~" (rx (or ">" "=" "-" "@" "~>" (+ "~"))))
-							;; __ ___ ____ _|_ __|____|_
-							("_" (rx (+ (or "_" "|"))))
-							;; Fira code: 0xFF 0x12
-							("0" (rx (and "x" (+ (in "A-F" "a-f" "0-9")))))
-							;; Fira code:
-							"Fl"  "Tl"  "fi"  "fj"  "fl"  "ft"
-							;; The few not covered by the regexps.
-							"{|"  "[|"  "]#"  "(*"  "}#"  "$>"  "^="))
-  ;; Enables ligature checks globally in all buffers. You can also do it
-  ;; per mode with `ligature-mode'.
-  (global-ligature-mode t))
+ (if (display-graphic-p)
+	(add-hook 'prog-mode-hook #'fira-code-mode)))
 
 ;; doom theme
 (use-package doom-themes
@@ -801,3 +735,20 @@
 (use-package doc-view
   :custom
   (doc-view-mupdf-use-svg t))
+
+;; python
+(use-package python-mode
+  :ensure t
+  :hook
+  (python-mode . lsp))
+
+(use-package pyvenv
+    :ensure t)
+
+(use-package auto-virtualenv
+  :ensure t
+  :init
+  :config
+  (add-hook 'python-mode-hook 'auto-virtualenv-set-virtualenv)
+  (add-hook 'projectile-after-switch-project-hook 'auto-virtualenv-set-virtualenv)  ;; If using projectile
+)
